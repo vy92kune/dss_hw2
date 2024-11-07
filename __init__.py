f8184c613245e3c27aec5b4ae26201f75984a096
vy92kune <kingsmendk2001@gmail.com>
2024-11-07 04:01:43 -0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""
+This is the main module for the math_quiz package.
+"""
+
+from .math_quiz import Quiz
+
+__all__ = ['Quiz']
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='1.0.1',  # Replace with your desired version
+    description='A simple math quiz game',
+    author='Dilip Kumar Madesh',
+    author_email='dilip.madesh@fau.de',
+    packages=find_packages(),
+)
